b8df9527f754e8aa84f88353c7926c0618b7d9c6
Carl Gregg <cgregg16206@gmail.com>
2022-09-29 16:24:47 -0500

    Added Swagger definitions

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors"); 
 const morgan = require("morgan"); 
 const low = require("lowdb"); 
+const swaggerUI = require("swagger-ui-express"); 
+const swaggerJsDoc = require("swagger-jsdoc"); 
 const booksRouter = require("./routes/books"); 
 
 const PORT = process.env.PORT || 4000; 
@@ -11,9 +13,33 @@ const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json"); 
 const db = low(adapter); 
 
+const app = express(); 
+
 db.defaults({ books: [] }).write(); 
 
-const app = express(); 
+const options = {
+  definition : {
+    openapi: "3.0.0", 
+    info: {
+      title: "Library API", 
+      version: "1.0.0", 
+      description: "A simple Express Library API"
+    }, 
+    servers: [
+      {
+        url: "http://localhost:4000"
+      }
+    ]
+  },
+  apis: [
+    "./routes/*.js"
+  ]
+}
+
+const specs = swaggerJsDoc(options); 
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 
 app.db = db; 
 
